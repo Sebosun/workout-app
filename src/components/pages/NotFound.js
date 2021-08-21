9d8e9b626bbfa5bd92f48ff7d81b66d301f1b0ae
Sebosun <sebos@protonmail.com>
2021-08-21 20:01:50 +0200

    redirect on notfound

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
 
 const NotFound = (props) => {
-  return <h1 style={{ textAlign: "center" }}>Page not found</h1>;
+  const [redirect, setRedirect] = useState(null);
+
+  setTimeout(() => {
+    setRedirect(<Redirect to="/main" />);
+  }, 1000);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Page not found</h1>
+      <p> You will be redirected to the main page shortly</p>
+      {redirect}
+    </div>
+  );
 };
 
 export default NotFound;
