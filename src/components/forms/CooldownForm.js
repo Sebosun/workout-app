108efd9fe3b08b8c7a9ec05170eb1fe3a431a1be
Sebosun <sebos@protonmail.com>
2021-08-21 20:01:00 +0200

    revamp of settings menu

--- src/components/forms/CooldownForm.js
+++ src/components/forms/CooldownForm.js
@@ -1,35 +1,16 @@
-import { useState } from "react";
-import Button from "../ui/Button";
-import { useDispatch, useSelector } from "react-redux";
-import { settingsActions } from "../../store/settings-slice";
+import React from "react";
 
 const CooldownForm = (props) => {
-  const rdxCd = useSelector((state) => state.settings.cooldown);
-  const [cooldown, setCooldown] = useState(rdxCd);
-
-  const dispatch = useDispatch();
-  const submitHandler = (e) => {
-    e.preventDefault();
-    dispatch(settingsActions.changeCooldown(cooldown));
-  };
-
-  const cooldownHandler = (e) => {
-    setCooldown(e.target.value);
-  };
-
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="cooldown">
-        Cooldown:
-        <input
-          value={cooldown}
-          onChange={cooldownHandler}
-          type="number"
-          name="cooldown"
-        />
-      </label>
-      <Button>Submit</Button>
-    </form>
+    <label htmlFor="cooldown">
+      Cooldown:
+      <input
+        value={props.cooldown}
+        onChange={props.cooldownHandler}
+        type="number"
+        name="cooldown"
+      />
+    </label>
   );
 };
 
--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -1,10 +1,31 @@
 import React from "react";
 import CooldownForm from "../forms/CooldownForm";
+import { useState } from "react";
+import Button from "../ui/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { settingsActions } from "../../store/settings-slice";
 
-const Settings = (props) => {
+const Settings = () => {
+  const cooldownRedux = useSelector((state) => state.settings.cooldown);
+  const dispatch = useDispatch();
+
+  const [cooldown, setCooldown] = useState(cooldownRedux);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(settingsActions.changeCooldown(cooldown));
+  };
+
+  const cooldownHandler = (e) => {
+    setCooldown(e.target.value);
+  };
+
   return (
     <div>
-      <CooldownForm />
+      <form onSubmit={submitHandler}>
+        <CooldownForm value={cooldown} cooldownHandler={cooldownHandler} />
+        <Button> Submit</Button>
+      </form>
     </div>
   );
 };
