69c2268dcfc3507ea4db2b3b070c488d568f2052
Sebosun <sebos@protonmail.com>
2021-08-24 18:52:47 +0200

    making this conditional more readable

--- src/components/pages/WorkoutPage.js
+++ src/components/pages/WorkoutPage.js
@@ -7,16 +7,15 @@ const WorkoutPage = (props) => {
   const [workoutStarted, setWorkoutStarted] = useState(false);
   const workoutRdx = useSelector((state) => state.workout.workout);
 
-  console.log(workoutRdx);
   const handleStart = () => {
     setWorkoutStarted((prev) => !prev);
   };
+
   return (
     <>
-      {workoutStarted ? (
-        <Workout data={workoutRdx} />
-      ) : (
-        <Button onClick={handleStart}>
+      {workoutStarted && <Workout data={workoutRdx} />}
+      {!workoutStarted && (
+        <Button style={{ margin: "100px auto" }} onClick={handleStart}>
           <h1> Start workout</h1>
         </Button>
       )}
