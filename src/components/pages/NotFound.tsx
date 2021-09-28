a2456f7088b01380887dd495457198dc98afc92b
Sebosun <sebos@protonmail.com>
2021-09-28 19:50:45 +0200

    NotFound Page ported

---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
+import Flex from "../ui/Flex";
+
+const NotFound = () => {
+  const [redirect, setRedirect] = useState<React.ReactElement>();
+
+  setTimeout(() => {
+    setRedirect(<Redirect to="/main" />);
+  }, 4000);
+
+  return (
+    <Flex column={true}>
+      <h1>Page not found</h1>
+      <p> You will be redirected to the main page shortly</p>
+      <p>
+        Dolor eligendi dolorum voluptatibus eaque vero Culpa esse quam optio
+        repellat molestiae. Quisquam corrupti sapiente sapiente exercitationem
+        beatae Neque autem dignissimos ratione qui optio Recusandae dolorem
+        consequuntur distinctio tempora sint.
+      </p>
+
+      {redirect}
+    </Flex>
+  );
+};
+
+export default NotFound;
