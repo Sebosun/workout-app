66ed98fde85e5bad6086cf1dba9bfa6109181a00
Sebosun <sebos@protonmail.com>
2021-09-28 12:29:33 +0200

    Card wrapper ported

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Card.module.css";
+
+interface Props {
+  className: string;
+  children: React.ReactNode;
+}
+
+const Card = ({ className, children }: Props) => {
+  return <div className={`${classes.card} ${className}`}>{children}</div>;
+};
+
+export default Card;
