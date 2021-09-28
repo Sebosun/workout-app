6a7cec34d32c49bbb282952758009e68f84d3a75
Sebosun <sebos@protonmail.com>
2021-09-28 14:56:10 +0200

    Header port

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,22 @@
+import classes from "./Header.module.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <ul className={classes.list}>
+        <li>
+          <Link to="/">Main</Link>
+        </li>
+        <li>
+          <Link to="/workout">Workout</Link>
+        </li>
+        <li>
+          <Link to="/settings">Settings</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
