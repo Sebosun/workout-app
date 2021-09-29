25ca0e74802bb85e6b5648de402a2c690d366077
Sebosun <sebos@protonmail.com>
2021-09-29 18:51:09 +0200

    ported to ts index store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import settingsActions from "./settings-slice";
+import settingsReducer from "./settings-slice";
 import timerActions from "./timer-slice";
 import workoutActions from "./workout-slice";
 
 const store = configureStore({
   reducer: {
-    settings: settingsActions.reducer,
+    settings: settingsReducer,
     timer: timerActions.reducer,
     workout: workoutActions.reducer,
   },
 });
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
