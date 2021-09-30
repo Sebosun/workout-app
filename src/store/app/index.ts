3d208246536974d2591e144708aea02baffc310e
Sebosun <sebos@protonmail.com>
2021-09-30 22:01:16 +0200

    typescript-friendly custom hooks for redux

--- src/store/app/index.ts
+++ src/store/app/index.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import settingsReducer from "./settings-slice";
-import timerReducer from "./timer-slice";
-import workoutActions from "./workout-slice";
+import settingsReducer from "../slices/settings-slice";
+import timerReducer from "../slices/timer-slice";
+import workoutActions from "../slices/workout-slice";
 
 const store = configureStore({
   reducer: {
