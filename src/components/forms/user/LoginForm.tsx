cec146e32e7a949ef7dd09f5a84ed538e14afe53
Sebosun <sebos@protonmail.com>
2021-10-01 18:30:01 +0200

    wrapping form with a portal modal

--- src/components/forms/user/LoginForm.tsx
+++ src/components/forms/user/LoginForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Button from "../../ui/Button";
+import PortalWrapper from "../../ui/PortalWrapper";
 
 interface LoginData {
   username: string;
@@ -15,39 +16,46 @@ const LoginForm = () => {
   const [loginData, setLoginData] = useState(initialState);
   const { username, password } = loginData;
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setLoginData((prevState) => ({
       ...prevState,
       [event.target.id]: event.target.value,
     }));
   };
 
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    console.log(username, password);
+  };
+
   return (
-    <form>
-      <label htmlFor="username">
-        Username:
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={username}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label htmlFor="password">
-        Password:
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={password}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <Button> Submit </Button>
-    </form>
+    <PortalWrapper>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="username">
+          Username:
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <Button> Submit </Button>
+      </form>
+    </PortalWrapper>
   );
 };
 
