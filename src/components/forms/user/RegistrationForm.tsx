7c84e1a3de742e3804f731c2da6ceb6b5738dcf4
Sebosun <sebos@protonmail.com>
2021-10-01 19:05:25 +0200

    registration form component

--- src/components/forms/user/RegistrationForm.tsx
+++ src/components/forms/user/RegistrationForm.tsx
@@ -1,7 +1,74 @@
-import React, { ReactElement } from "react";
+import React, { useState } from "react";
+import Button from "../../ui/Button";
+import PortalWrapper from "../../ui/PortalWrapper";
 
-const RegistrationForm = (): ReactElement | null => {
-  return <p>dupa</p>;
+interface RegistrationData {
+  username: string;
+  password: string;
+  email: string;
+}
+
+const initialState: RegistrationData = {
+  username: "",
+  password: "",
+  email: "",
+};
+
+const RegistrationForm = () => {
+  const [loginData, setLoginData] = useState(initialState);
+  const { username, password, email } = loginData;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setLoginData((prevState) => ({
+      ...prevState,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    console.log(username, password, email);
+  };
+
+  return (
+    <PortalWrapper>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="username">
+          Username:
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+        <Button> Submit </Button>
+      </form>
+    </PortalWrapper>
+  );
 };
 
 export default RegistrationForm;
