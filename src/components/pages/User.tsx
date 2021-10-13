be42f8f29ac7a9a65226b16647ee7a45a3bf9b91
Sebosun <sebos@protonmail.com>
2021-10-13 20:50:22 +0200

    making this menu more responsive

--- src/components/pages/User.tsx
+++ src/components/pages/User.tsx
@@ -3,9 +3,9 @@ import {Link} from "react-router-dom";
 import ExerciseForm from '../forms/workout/ExerciseForm'
 
 export default function User(): ReactElement | null {
-  const linkClasses = "flex items-center self-center justify-center h-48 text-4xl border-2 border-solid text-center max-w-sm lg:max-w-lg rounded-xl border-crimson"
+  const linkClasses = "flex sm:flex-row items-center self-center justify-center h-48 text-4xl border-2 border-solid text-center max-w-sm lg:max-w-lg rounded-xl border-crimson"
   return (
-    <div className="object-center max-w-4xl mx-auto my-32 xl lg:my-52 place-content-stretch gap-4 grid grid-cols-2">
+    <div className="justify-center object-center max-w-4xl p-2 mx-auto my-32 xl lg:my-52 place-content-stretch gap-4 grid grid-row-2 sm:grid-cols-2">
       <Link to="/add-workout" className={`${linkClasses}`}>
         Add workout template
       </Link >
