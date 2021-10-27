1b023e989d8ee319f4bdce8ab3f06a6b9ff5d968
Sebosun <sebos@protonmail.com>
2021-10-27 16:31:49 +0200

    removing unecessary slice

--- src/store/app/index.ts
+++ src/store/app/index.ts
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import settingsReducer from "../slices/settings-slice";
 import timerReducer from "../slices/timer-slice";
 import workoutReducer from "../slices/workout-slice";
-import userReducer from "../slices/user-slice";
 import uiReducer from "../slices/ui-slice";
 
 const store = configureStore({
@@ -10,7 +9,6 @@ const store = configureStore({
     settings: settingsReducer,
     timer: timerReducer,
     workout: workoutReducer,
-    user: userReducer,
     ui: uiReducer,
   },
 });
