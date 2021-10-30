d0d75cbe2bba5291f886bda7a93fddae0771a687
Sebosun <sebos@protonmail.com>
2021-10-30 21:03:41 +0200

    fucntionality for set as current template in user settings

--- src/components/pages/User/CheckWorkoutTemplates.tsx
+++ src/components/pages/User/CheckWorkoutTemplates.tsx
@@ -1,21 +1,25 @@
-import { ReactElement, useEffect, useState } from "react"
+import { ReactElement, useEffect, useState } from "react";
 
 import firebase from "firebase/app";
 import "firebase/firestore";
-import {useAppDispatch} from "../../../store/app/hooks";
+import { useAppDispatch } from "../../../store/app/hooks";
 import DisplayWorkoutTemplatePreview from "../../workout/workoutTemplates/DisplayWorkoutTemplatePreview";
+import { changeCurrentWorkoutTemplate } from "../../../store/slices/settings-slice";
+import { displaySuccess } from "../../../store/slices/ui-slice";
 
 interface previewItem {
   name: string;
-  date: Date,
+  date: Date;
   workout: [];
 }
 
 export default function CheckWorkoutTemplates(): ReactElement | null {
-  const [templateData, setTemplateData] = useState<firebase.firestore.DocumentData[]|null>(null) 
-  const [preview, setPreview] = useState<previewItem | null>()
+  const [templateData, setTemplateData] = useState<
+    firebase.firestore.DocumentData[] | null
+  >(null);
+  const [preview, setPreview] = useState<previewItem | null>();
   const user = firebase.auth().currentUser;
-  const dispatch = useAppDispatch() 
+  const dispatch = useAppDispatch();
 
   // TODO getitng unique id for array later
   useEffect(() => {
@@ -35,7 +39,7 @@ export default function CheckWorkoutTemplates(): ReactElement | null {
             workoutTemplates.push(doc.data());
           });
 
-          console.log(workoutTemplates)
+          console.log(workoutTemplates);
           setTemplateData([...workoutTemplates]);
         }
       } catch (err: any) {
@@ -47,33 +51,35 @@ export default function CheckWorkoutTemplates(): ReactElement | null {
     }
   }, [dispatch, user]);
 
-
   //TODO: Figure out the type for this
   const onShowPreview = (item: any) => {
-    setPreview(item)
-  }
+    setPreview(item);
+  };
 
-    const dupa: any = templateData?.map((item) => (
-      <div className="max-w-xl p-2 mx-auto my-4 lg:max-w-2xl">
-        <div key={item.name}>
-          <p>Name: {item.name}</p>
-          <p>
-            Date added:{" "}
-            {item.date.toDate().toDateString() +
-              " " +
-              item.date.toDate().toLocaleTimeString()}
-          </p>
-          <button
-            onClick={() => onShowPreview(item)}
-            value="dupa"
-            className="btn"
-          >
-            Show preview
-          </button>
-          <button className="btn" onClick={() => console.log('clicked')}>Set as current template</button>
-        </div>
+  const handleChangeTemplate = (name: string) => {
+    dispatch(changeCurrentWorkoutTemplate(name));
+    dispatch(displaySuccess(`Workout template succesfully changed to ${name}`));
+  };
+
+  const templatesMapped: any = templateData?.map((item, index) => (
+    <div className="max-w-xl p-2 mx-auto my-4 lg:max-w-2xl">
+      <div key={`${item.name}${index}`}>
+        <p>Name: {item.name}</p>
+        <p>
+          Date added:{" "}
+          {item.date.toDate().toDateString() +
+            " " +
+            item.date.toDate().toLocaleTimeString()}
+        </p>
+        <button onClick={() => onShowPreview(item)} className="btn">
+          Show preview
+        </button>
+        <button className="btn" onClick={() => handleChangeTemplate(item.name)}>
+          Set as current template
+        </button>
       </div>
-    ));
+    </div>
+  ));
 
   if (!templateData) {
     return <p>Loading...</p>;
@@ -82,11 +88,13 @@ export default function CheckWorkoutTemplates(): ReactElement | null {
       <div className="max-w-md p-2 mx-auto lg:max-w-xl">
         <h1 className="my-4 text-4xl text-center">{preview.name}</h1>
         <DisplayWorkoutTemplatePreview workout={preview.workout} />
-        <button onClick={() => setPreview(null)} className="btn"> Return </button>
+        <button onClick={() => setPreview(null)} className="btn">
+          {" "}
+          Return{" "}
+        </button>
       </div>
     );
   } else {
-    return <p>{dupa && dupa}</p>;
+    return <p>{templatesMapped && templatesMapped}</p>;
   }
-  
 }
