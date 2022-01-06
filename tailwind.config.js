3dd55489c89dab915465f138bec2680363c42523
Sebosun <sebos@protonmail.com>
2022-01-06 18:51:40 +0100

    purging in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
