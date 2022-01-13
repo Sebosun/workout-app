9fffefeab14c045319557c708fdc4aa50b2677a7
Sebosun <sebos@protonmail.com>
2022-01-13 18:50:20 +0100

    removing unecessary margins

--- src/components/pages/User/User.tsx
+++ src/components/pages/User/User.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function User(): ReactElement | null {
   const linkClasses =
-    "flex sm:flex-row items-center self-center justify-center h-48 text-4xl border-2 border-solid text-center max-w-sm lg:max-w-lg rounded-xl btn";
+    "flex sm:flex-row items-center self-center justify-center p-4 h-48 text-4xl border-2 border-solid text-center max-w-sm lg:max-w-lg rounded-xl btn";
 
   return (
-    <div className="grid object-center gap-4 justify-center place-content-stretch p-2 my-32 mx-auto max-w-4xl sm:grid-cols-2 lg:my-52 grid-row-2 xl">
+    <div className="justify-center object-center max-w-4xl p-8 mx-auto grid gap-4 place-content-stretch grid-rows-2">
       <Link to="/user/add-workout" className={`${linkClasses}`}>
         Add workout template
       </Link>
