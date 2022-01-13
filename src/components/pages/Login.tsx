bc4aac5670701a7defbdd6eca3a91136fad862b6
Sebosun <sebos@protonmail.com>
2022-01-13 17:42:03 +0100

    removing portal on registration

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -11,11 +11,7 @@ const Login = ({ mode }: LoginState) => {
   if (mode === "login") {
     return <LoginForm />;
   } else if (mode === "registration") {
-    return (
-      <PortalWrapper>
-        <RegistrationForm />
-      </PortalWrapper>
-    );
+    return <RegistrationForm />;
   } else {
     return null;
   }
